f67619819a1ba59f279ac0344ed809d3301a4111
ddong1851 <ddong1851@naver.com>
2021-12-27 23:55:13 +0900

    12-27

---
+++ practicejava/src/backjun1110.java
@@ -0,0 +1,25 @@
+package src;
+
+import java.util.Scanner;
+
+//백준 1110
+public class backjun1110 {
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		s.close();
+		
+		int temp = n;
+		int count = 0;
+		
+		while(true) {
+			n = ((n%10)*10)+(((n/10)+(n%10))%10);	
+			count ++;								
+			if(n==temp) { 							
+				System.out.println(count);
+				break;
+			}
+		}
+	}
+}
