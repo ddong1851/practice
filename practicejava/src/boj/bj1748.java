05af4d53f31ebe89896a93968f6b8f9cff5073be
ddong1851 <ddong1851@naver.com>
2022-01-08 23:48:04 +0900

    Create bj1748.java

---
+++ practicejava/src/boj/bj1748.java
@@ -0,0 +1,23 @@
+package src.boj;
+
+import java.util.Scanner;
+
+public class bj1748 {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        sc.close();
+
+        int count = 0;
+        int temp = 10;
+        int add = 1;
+        for(int i=1; i<=n; i++){
+            if(i>=temp){
+                add++;
+                temp *= 10;
+            }
+            count += add;
+        }
+        System.out.println(count);
+    }
+}
