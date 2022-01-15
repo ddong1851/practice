b404225a6f2cb6176a05e4bc2ae4e7fde069bf30
ddong1851 <ddong1851@naver.com>
2022-01-15 22:42:28 +0900

    1-15

--- practicejava/src/boj/Main.java
+++ practicejava/src/boj/Main.java
@@ -1,24 +1,34 @@
 package src.boj;
 
-import java.math.BigInteger;
 import java.util.Scanner;
 
-//DP로 풀어야함
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt(); // 100
-        int m = sc.nextInt(); // 6 
-        
-        BigInteger up = BigInteger.ONE;
-        BigInteger down = BigInteger.ONE;
-        for(int i=0; i<m; i++){
-            up = up.multiply(new BigInteger(String.valueOf(n-i)));
-            down = down.divide(new BigInteger(String.valueOf(i+1)));
-        }
-        
-        System.out.println(up.divide(down));
-               
-    } // end of main
-} // end of Main
+/**
+ * Main_백준_1932_정수삼각형_실버1_
+ * @author HD
+ *
+ */
+public class Main {
+	public static int [][] triangle;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		triangle = new int[n][n];
+		
+		//삼각형 값 초기화
+		for (int i = 0; i < n; i++) {
+			for(int j=0; j<=i; j++) {
+				triangle[i][j] = sc.nextInt();
+			}
+		}
+		
+		//삼각형을 초기화
+		for(int i=n-2; i>=0; i--) {
+			for(int j=0; j<=i; j++) {
+				
+			}
+		}
+	}
+
+}
