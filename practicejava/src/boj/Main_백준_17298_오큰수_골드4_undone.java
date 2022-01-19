6d7fb7c63f0e919d3deb4ce3eb37675472b1724f
ddong1851 <ddong1851@naver.com>
2022-01-19 23:14:16 +0900

    1-19

--- practicejava/src/boj/Main_백준_17298_오큰수_골드4_undone.java
+++ practicejava/src/boj/Main_백준_17298_오큰수_골드4_undone.java
@@ -1,10 +1,10 @@
-package src.boj;
+ package src.boj;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 
 /**
@@ -13,45 +13,57 @@ import java.util.StringTokenizer;
  *
  */
 //시간 제한 1초, 512MB, 
+
+//stack에 인덱스를 저장하고 해봐라.
 public class Main_백준_17298_오큰수_골드4_undone {
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		
+		// 스택
 		Stack<Integer> stack = new Stack<>();
+		// NGE 값들이 저장될 배열
 		int [] NGE = new int[n];
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int i=0;
+		// int형 데이터를 저장할 data
+		int [] data = new int[n];
+		String [] str = br.readLine().split(" ");
 		
 		//먼저 배열에 다 넣는다.
-		while(st.hasMoreTokens()) {
-			NGE[i] = Integer.parseInt(st.nextToken());
-			i++;
+		for(int i=0; i<n; i++) {
+			data[i] = Integer.parseInt(str[i]);
 		}
-		br.close();
 		
-		i=0;
-		while(true) {
-			// 값들을 비교해줄 변수, 내가 지금 가리키는 변수
-			int num = -1;
-			int temp = NGE[i];			
-			// 현재 조회중인 변수부터 비교, 
-			for(int j=i+1; j<n; j++) {
-				// 현재 가리키고 있는 변수보다 
-				if(temp<NGE[j]) {
-					num = NGE[j];
-					break; // for문 탈출
-				} 
+		// NGE배열을 -1로 초기화
+		Arrays.fill(NGE,-1);
+		
+		
+		// 0번째 인덱스를 스택에 넣는다.
+		stack.push(0);
+		int idx = 0;
+		// 스택이 비어있지 않을 때 계속 반복
+		while(!stack.isEmpty()) {
+			// 값을 비교해줄 temp 변수, 데이터의 첫번째 데이터로 초기화한다. 
+			int temp = data[idx++];
+			// 현재 비교대상인 값이랑, 스택의 맨 위 idx 값이랑 비교
+			for(int j=idx; j<idx; j++) {
+				stack.push(idx);
+				if(temp<data[stack.peek()]) {
+					
+				}
+				
 			}
-			stack.push(num);
-			if(i==n-1) {
-				break;
-			}
-			i++;		// 다음 칸 조회
+			stack.pop();
 		} // end of while
-		String str = stack.toString();
-		System.out.println(str.substring(1, str.length()-1));
+		
+		for(int i:NGE) {
+			System.out.print(i+" ");
+		}
+		/*
+TC
+4
+9 5 4 8
+		 */
+		br.close();
 	} // end of main
 } // end of class
