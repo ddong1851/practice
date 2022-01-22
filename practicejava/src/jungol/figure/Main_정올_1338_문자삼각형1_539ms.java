c69acf615b98f94a714ac8bba11a63b9469d5785
ddong1851 <ddong1851@naver.com>
2022-01-23 01:34:28 +0900

    1-22

--- practicejava/src/jungol/figure/Main_정올_1338_문자삼각형1_539ms.java
+++ practicejava/src/jungol/figure/Main_정올_1338_문자삼각형1_539ms.java
@@ -3,33 +3,39 @@ package src.jungol.figure;
 import java.util.Arrays;
 import java.util.Scanner;
 
-/** Main_정올_1338_문자삼각형1_*/
-public class Main {
+/** Main_정올_1338_문자삼각형1_539ms*/
+public class Main_정올_1338_문자삼각형1_539ms {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		
 		
 		char [][] figure = new char[n][n];
+		
 		// 반칸으로 채움
 		for(char[] c:figure) {
 			Arrays.fill(c, ' ');
 		}
 		
+		
+		
 		// 계속 제자리에 초기화되고 있다.
 		char p = 'A';
-		for(int i=0; i<n; i++) {
-			for(int j=i; j<n; j++) {
-				figure[j][n-j-1] =p++;
+		int k=0;
+		for(int i=1; i<=n; i++) {
+			for(int j=i; j<=n; j++) {
+				figure[j-1]
+						[n-j+k] =p++;
 				if(p>'Z') p='A';
 			}
+			k++;
 		}
 		
 		
 		
 		for(char[] c:figure) {
 			for(char a:c) {
-				System.out.print(a);
+				System.out.print(a+" ");
 			}
 			System.out.println();
 		}
@@ -38,11 +44,11 @@ public class Main {
 		/*
 		 * ex. n=5
 		 * 				공백	출력
-		 * 			*	4	1
-		 * 		  * *	3	2
-		 * 		* * *	2	3
-		 * 	  * * * *	1	4
-		 *  * * * * *	0	5
+		 * 			*	4	1					0 n-1
+		 * 		  * *	3	2    			1 n-2 1 n-1
+		 * 		* * *	2	3	  		2 n-3 2 n-2
+		 * 	  * * * *	1	4		3 n-4
+		 *  * * * * *	0	5	4 n-5
 		 * 
 		 * 
 		 * 
---
+++ practicejava/src/jungol/figure/Main_정올_1339_뮨자삼각형2_423ms.java
@@ -0,0 +1,58 @@
+package src.jungol.figure;
+
+import java.util.Scanner;
+
+/**Main_정올_1339_뮨자삼각형2_423ms*/
+public class Main_정올_1339_뮨자삼각형2_423ms {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		// 상수를 입력받을 변수
+		int n = sc.nextInt();
+		if(n<=0 || n>100 || n%2==0) {
+			System.out.println("INPUT ERROR");
+			return;
+		}
+		
+		// 2차원 배열을 만들고, 해당 배열에 값을 넣을 예정
+		int r = n/2; // 열의 길이는 높이의 절반
+		char [][] figure = new char[n][r+1];
+		// 배열에 들어갈 값의 초기값
+		char p = 'A';
+		
+		// 가로 길이 만큼 반복 
+		for(int i=0; i<=r; i++) {
+			// 출력 개수가 +2씩 늘기 때문에 2*i, 그리고 홀수개 출력을 위한 +1
+			int range = 2*i+1;
+			for(int j=0;j<range;j++) {
+				//		가변	   고정
+				figure[r-i+j][r-i] = p++;
+				// 출력할 때 알파벳 범위를 벗어나면 A로 초기화
+				if(p>'Z') p='A';
+			}
+		}
+		
+		// 출력
+		for(char[] c:figure) {
+			for(char a:c) {
+				System.out.print(a+" ");
+			}
+			System.out.println();
+		}
+		
+		
+		/*
+		 * 
+		 * 	  5	 
+		 *    6 2
+		 *    7 3 1
+		 *	  8	4 
+		 *    9
+		 * 
+		 */
+		
+		
+	}
+
+}
