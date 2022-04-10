766a9207971bc85ee199c0f1642b4999e8ec2b09
ddong1851 <ddong1851@naver.com>
2022-04-10 22:54:45 +0900

    04-10

---
+++ practicejava/src/boj/undone/Main_백준_11658_구간합구하기3_플레5_시간초과.java
@@ -0,0 +1,33 @@
+package src.boj.undone;
+
+import java.io.*;
+import java.util.*;
+
+/** Main_백준_11658_구간합구하기3_플레5_시간초과 */
+public class Main_백준_11658_구간합구하기3_플레5_시간초과 {
+	
+	private static int N, M;
+	public static void main(String[] args) throws Exception {
+		
+		/*
+		 * 방법론 자체가 틀린 문제
+		 * 세그먼트 트리라는 아이템을 사용해야 한다고 한다...
+		 * 아직 할줄 모르는 부분이긴 하다.
+		 */
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+	} // end of main 
+	
+} // end of class 
+
+
+
+
+
+
